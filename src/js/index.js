7a3327818ab2efebbca9fa1701ab4819750ec2fb
Eduard Rudniev <eduardrudnev97@gmail.com>
2020-03-18 20:22:03 +0300

    load pc version

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider"
+
+
+
+// const slider = tns({
+//     container: '.first-block__slider-content',
+//     loop: true,
+//     items: 1,
+//     slideBy: 'page',
+//     nav: false,    
+//     autoplay: true,
+//     speed: 400,
+//     autoplayButtonOutput: false,
+//     mouseDrag: true,
+//     lazyload: true,
+
+//   });
